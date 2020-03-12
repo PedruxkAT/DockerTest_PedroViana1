ed01b461b62a84bd991f22ada5adc417c3ab91e4
Pedro Viana <pedro.viana@agilethought.com>
2020-03-12 17:59:11 -0600

    test

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DockerTest_PedroViana.Models;
+
+namespace DockerTest_PedroViana.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Microsoft.Extensions.Configuration.IConfiguration _configuration;
+        public HomeController(Microsoft.Extensions.Configuration.IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.EnvMensaje = _configuration.GetSection("AppSettings").GetSection("storename").Value;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
